849a44e6be75135c0edae72be2da510a287972e3
Hyun Young Lee <hyunyounglee@Hyuns-MacBook-Pro.local>
2021-04-22 23:29:27 +0100

    Update EventList.test.js to use mockData

--- src/_tests_/EventList.test.js
+++ src/_tests_/EventList.test.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import EventList from '../EventList';
 import Event from '../Event';
+import { mockData } from '../mockdata';
 
 describe('<EventList /> component', () => {
     test('render correct number of events', () => {
         const EventListWrapper = shallow(
-            <EventList events={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }]} />
+            <EventList events={mockData} />
         );
-        expect(EventListWrapper.find(Event)).toHaveLength(5);
+        expect(EventListWrapper.find(Event)).toHaveLength(mockData.length);
     });
 });
