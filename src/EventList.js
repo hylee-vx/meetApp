eb89f9cef3e9f4b4fd2d9f5957e43f6df67be423
Hyun Young Lee <hyunyounglee@Hyuns-MacBook-Pro.local>
2021-04-22 11:55:56 +0100

    Write unit tests and components to show default list of events
    - test Feature 1, Scenario 1: When user hasn't searched for a city, show upcoming events from all cities
    - unit test 1: shallow render list of <Events> in <EventList> component, using mock data in EventList.test.js file; PASSED
    - unit test 2: shallow render <EventList> in <App> component; PASSED
    - components written as functional components rather than class components as in exercise

---
+++ src/EventList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Event from './Event';
+
+const EventList = props => {
+    const { events } = props;
+
+    return (
+        <ul className="EventList">
+            {events.map(event =>
+                <li key={event.id}>
+                    <Event event={event} />
+                </li>
+            )}
+        </ul>
+    );
+};
+
+export default EventList;
