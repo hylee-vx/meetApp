94198cfbcedf0b9db0828bf24349c3f48fd8f121
Hyun Young Lee <hyunyounglee@Hyuns-MacBook-Pro.local>
2021-04-23 14:33:55 +0100

    Tests to show/hide event details
    - Feature 2, Scenario 1: An event element is collapsed by default
    - Scenario 2: User can expand an event to see its details
    - Scenario 3: User can collapse an event to hide its details
    - all unit tests PASSED
    - include toggling content on button ie 'Show details' when event collapsed, 'Hide details' when event expanded

--- src/Event.js
+++ src/Event.js
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Event = () => {
-    return <div></div>;
+class Event extends Component {
+    state = {
+        showDetails: false
+    };
+
+    handleClick = () => {
+        this.setState(previousState => ({
+            showDetails: !previousState.showDetails
+        }));
+    }
+
+    render() {
+        const { event } = this.props;
+        const { showDetails } = this.state;
+
+        return (
+            <div className="event">
+                <div className="event-summary">
+                    <h2>{event.summary}</h2>
+                    <p>{event.start.dateTime}</p>
+                    <p>{event.start.timeZone}</p>
+                    <p>{event.location}</p>
+                </div>
+
+                {showDetails
+                    ? <div>
+                        <div className="event-details">
+                            <a href={event.htmlLink}>See details on Google Calendar</a>
+                            <p>{event.description}</p>
+                        </div>
+                        <button
+                            className="hide-details-btn"
+                            onClick={() => this.handleClick()}
+                        >Hide details</button>
+                    </div>
+                    : <button
+                        className="show-details-btn"
+                        onClick={() => this.handleClick()}
+                    >Show details</button>
+                }
+
+
+            </div>
+        );
+    }
 };
 
 export default Event;
