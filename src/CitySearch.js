40e3760221a43248ee13fbc2e585ba6b3b0c3137
Hyun Young Lee <hyunyounglee@Hyuns-MacBook-Pro.local>
2021-04-25 22:12:07 +0100

    Update CitySearch to remove suggestions list after choosing suggested city
    - Feature 1, Scenario 3: User can select a city from the suggested list
    - tests to remove list of suggested cities after user has clicked on a city; PASSED

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 class CitySearch extends Component {
     state = {
         query: '',
-        suggestions: []
+        suggestions: [],
+        showSuggestions: undefined
     };
 
     handleInputChange = event => {
@@ -20,7 +21,8 @@ class CitySearch extends Component {
 
     handleItemClick = suggestion => {
         this.setState({
-            query: suggestion
+            query: suggestion,
+            showSuggestions: false
         });
 
         this.props.updateEvents(suggestion);
@@ -35,9 +37,13 @@ class CitySearch extends Component {
                     className="city"
                     value={this.state.query}
                     onChange={this.handleInputChange}
+                    onFocus={() => { this.setState({ showSuggestions: true }) }}
                 />
 
-                <ul className="suggestions">
+                <ul
+                    className="suggestions"
+                    style={this.state.showSuggestions ? {} : { display: 'none' }}
+                >
                     {this.state.suggestions.map(suggestion => (
                         <li
                             key={suggestion}
